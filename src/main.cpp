57a08eea47cf1f0ebf37d9a10a89864f22749e68
xdragunov <ydragoni@gmail.com>
2018-02-07 19:09:15 +0100

    Event class implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "person.h"
+#include "event.h"
 
 
 int main()//(int argc, char *argv[])
@@ -7,6 +8,9 @@ int main()//(int argc, char *argv[])
     std::cout << "Hello World!" << std::endl;
     Person::printAll();
     Person person(1);
+    std::cout << "Available events:" << std::endl;
+	Event::printAll();
+	std::cout << "Events list end." << std::endl;
     std::cout << person.getName() << std::endl;
     std::cout << person;
     char test;
