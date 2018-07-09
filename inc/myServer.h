694616f75a373c7e83dde8ae1d8b274dc7ee6f47
Jarmila Šágiová <jarmila.sagiova@mallgroup.com>
2018-07-09 20:32:13 +0200

    create server

---
+++ inc/myServer.h
@@ -0,0 +1,33 @@
+#ifndef MY_SERVER_H
+#define MY_SERVER_H
+
+#include "cpprest/json.h"
+#include "cpprest/http_listener.h"
+#include "cpprest/uri.h"
+#include "cpprest/asyncrt_utils.h"
+
+using namespace web;
+using namespace http;
+using namespace utility;
+using namespace http::experimental::listener;
+
+class MyServer
+{
+public:
+	MyServer() {}
+	MyServer(utility::string_t url);
+
+	pplx::task<void> open() { return m_listener.open(); }
+	pplx::task<void> close() { return m_listener.close(); }
+
+private:
+
+	void handle_get(http_request message);
+	void handle_put(http_request message);
+	void handle_post(http_request message);
+	void handle_delete(http_request message);
+
+	http_listener m_listener;
+};
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,71 @@
 #include <iostream>
 #include "person.h"
 #include "sql.h"
-#include "event.h"
 #include "sqlerror.h"
+#include "myServer.h"
 
+using namespace web;
+using namespace http;
+using namespace utility;
+using namespace http::experimental::listener;
 
-int main()//(int argc, char *argv[])
+std::unique_ptr<MyServer> g_http;
+
+void on_initialize(const string_t& address)
 {
-	try
-	{
-        std::cout << "Hello World!" << std::endl;
-        Person::printAll();
-        Person person(1);
-        std::cout << person.getName() << std::endl;
-        std::cout << person;		
-	}
-	catch (SqlError & e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	char test;
-	std::cin >> test;
+    // Build our listener's URI from the configured address and the hard-coded path "MyServer/Action"
+
+    uri_builder uri(address);
+    uri.append_path(U("MyServer/Action/"));
+
+    auto addr = uri.to_uri().to_string();
+	g_http = std::unique_ptr<MyServer>(new MyServer(addr));
+	g_http->open().wait();
+    
+    ucout << utility::string_t(U("Listening for requests at: ")) << addr << std::endl;
+
+    return;
+}
+
+void on_shutdown()
+{
+	g_http->close().wait();
+    return;
+}
+
+int main(int argc, char *argv[])
+{
+
+    utility::string_t port = U("34568");
+    if(argc == 2)
+    {
+        port = argv[1];
+    }
+
+    utility::string_t address = U("http://localhost:");
+    address.append(port);
+
+    on_initialize(address);
+    std::cout << "Press ENTER to exit." << std::endl;
+
+    std::string line;
+    std::getline(std::cin, line);
+
+    on_shutdown();
     return 0;
+	// try
+	// {
+    //     std::cout << "Hello World!" << std::endl;
+    //     Person::printAll();
+    //     Person person(1);
+    //     std::cout << person.getName() << std::endl;
+    //     std::cout << person;		
+	// }
+	// catch (SqlError & e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
+	// char test;
+	// std::cin >> test;
+    //return 0;
 }
